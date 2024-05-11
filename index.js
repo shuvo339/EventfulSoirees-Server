f1f26db922dc7e8edb254069c4c6e7a40f448f44
shuvo339 <shubhroshafiullah@gmail.com>
2024-05-11 11:38:33 +0600

    delete method implemented

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
       const result = await servicesCollection.insertOne(service);
       res.send(result); 
     })
+
+    app.delete('/services/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await servicesCollection.deleteOne(query);
+      res.send(result); 
+    })
     
 
     //Booking related api
